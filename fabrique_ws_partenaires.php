fedf1290cb5dad58ee02aa7a09633fc476a4871b
Rainer <rainer@websimple.be>
2014-03-25 17:45:57 +0100

    import initial

---
+++ fabrique_ws_partenaires.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *  Fichier généré par la Fabrique de plugin v5
+ *   le 2014-03-25 16:01:35
+ *
+ *  Ce fichier de sauvegarde peut servir à recréer
+ *  votre plugin avec le plugin «Fabrique» qui a servi à le créer.
+ *
+ *  Bien évidemment, les modifications apportées ultérieurement
+ *  par vos soins dans le code de ce plugin généré
+ *  NE SERONT PAS connues du plugin «Fabrique» et ne pourront pas
+ *  être recréées par lui !
+ *
+ *  La «Fabrique» ne pourra que régénerer le code de base du plugin
+ *  avec les informations dont il dispose.
+ *
+**/
+
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+$data = array (
+  'fabrique' => 
+  array (
+    'version' => 5,
+  ),
+  'paquet' => 
+  array (
+    'nom' => 'Web Simple Partenaires',
+    'slogan' => '',
+    'description' => '',
+    'prefixe' => 'ws_partenaires',
+    'version' => '1.0.0',
+    'auteur' => 'Rainer Müller',
+    'auteur_lien' => 'http://websimple.be',
+    'licence' => 'GNU/GPL',
+    'categorie' => 'squelette',
+    'etat' => 'dev',
+    'compatibilite' => '[3.0.16;3.0.*]',
+    'documentation' => '',
+    'administrations' => '',
+    'schema' => '1.0.0',
+    'formulaire_config' => '',
+    'formulaire_config_titre' => '',
+    'inserer' => 
+    array (
+      'paquet' => '',
+      'administrations' => 
+      array (
+        'maj' => '',
+        'desinstallation' => '',
+        'fin' => '',
+      ),
+      'base' => 
+      array (
+        'tables' => 
+        array (
+          'fin' => '',
+        ),
+      ),
+    ),
+    'scripts' => 
+    array (
+      'pre_copie' => '',
+      'post_creation' => '',
+    ),
+    'exemples' => 'on',
+  ),
+  'objets' => 
+  array (
+  ),
+  'images' => 
+  array (
+    'paquet' => 
+    array (
+      'logo' => 
+      array (
+        0 => 
+        array (
+          'extension' => '',
+          'contenu' => '',
+        ),
+      ),
+    ),
+    'objets' => 
+    array (
+    ),
+  ),
+);
+
+?>
